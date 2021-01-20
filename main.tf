cc620f12ad4c82d3b428819682bbbb58ccdaa70c
jennings <jennings@circleci.com>
2021-01-20 15:40:26 +0900

    Fix whitespace for tf fmt.

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 resource "tls_private_key" "this" {
-  algorithm     = "RSA"
-  rsa_bits      = 4096
+  algorithm = "RSA"
+  rsa_bits  = 4096
 }
 
 resource "aws_key_pair" "this" {
-  key_name      = var.key_name
-  public_key    = tls_private_key.this.public_key_openssh
+  key_name   = var.key_name
+  public_key = tls_private_key.this.public_key_openssh
 }

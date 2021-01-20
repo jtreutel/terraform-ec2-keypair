897de55e71cfe0a05d1bda05113bf396e7cd440a
jennings <jennings@circleci.com>
2021-01-20 11:28:40 +0900

    Finished script.

--- .test/render_template.py
+++ .test/render_template.py
@@ -1,4 +1,4 @@
-import glob, yaml
+import re, os, glob, yaml
 from jinja2 import Environment, FileSystemLoader
 
 # Load vars that will be used to render Terraform Jinja templates
@@ -6,17 +6,21 @@ templatevars_yml = open("templatevars.yml")
 templatevars = yaml.load(templatevars_yml, Loader=yaml.FullLoader)
 
 #Set directory in which jinja2 will look for templates
-env = Environment(loader=FileSystemLoader('templates'))
+env = Environment(loader=FileSystemLoader('')) #default dir is ./templates
 
 # Create list of all jinja files in templates dir
 template_file_list = glob.glob('./templates/*.j2')
 #print(template_file_list) #debug
 
-for filename in template_file_list:
-    template = env.get_template(filename)
+if not os.path.exists('rendered'):
+    os.makedirs('rendered')
+
+for file_fullpath in template_file_list:
+    template = env.get_template(file_fullpath)
 
     parsed_template = template.render(templatevars)
     #print(parsed_template) #debug
 
-    with open(''.join("./rendered/", filename.removesuffix('.j2')), "w") as fh:
+    filename = re.search(r"(?<=\.\/templates\/).*?(?=\.j2)", file_fullpath).group(0)
+    with open(''.join(["./rendered/", filename.removesuffix('.j2')]), "w") as fh:
         fh.write(parsed_template)
